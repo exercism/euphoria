dc7e0728d97a5c2632b4d1909b07477bd992f51a
Glenn Jackman <glenn.jackman@gmail.com>
2024-04-12 09:21:11 -0400

    use OK and ERR enum instead of strings. update run.ex to take a lib dir from the solution

--- exercises/practice/all-your-base/.meta/example.ex
+++ exercises/practice/all-your-base/.meta/example.ex
@@ -1,44 +1,46 @@
+include lib/errors.e
+
 public function rebase(integer input_base, sequence input_digits, integer output_base)
     sequence result = to_decimal(input_base, input_digits)
-    if equal(result[1], "err") then
+    if result[1] = ERR then
         return result
+    else
+        return to_base(output_base, result[2])
     end if
-
-    return to_base(output_base, result[2])
 end function
 
 
 function to_decimal(integer ibase, sequence digits)
     if ibase < 2 then
-        return {"err", "input base must be >= 2"}
+        return {ERR, "input base must be >= 2"}
     end if
 
     integer dec = 0
     for i = 1 to length(digits) do
         if digits[i] < 0 or digits[i] >= ibase then
-            return {"err", "all digits must satisfy 0 <= d < input base"}
+            return {ERR, "all digits must satisfy 0 <= d < input base"}
         end if
 
         dec = dec * ibase + digits[i]
     end for
 
-    return {"ok", dec}
+    return {OK, dec}
 end function
 
 function to_base(integer obase, integer dec)
     if obase < 2 then
-        return {"err", "output base must be >= 2"}
-    end if
-
-    if dec = 0 then
-        return {"ok", {0}}
+        return {ERR, "output base must be >= 2"}
     end if
 
     sequence digits = {}
-    while dec > 0 do
-        digits = prepend(digits, remainder(dec, obase))
-        dec = floor(dec / obase)
-    end while
+    if dec = 0 then
+        digits = {0}
+    else
+        while dec > 0 do
+            digits = prepend(digits, remainder(dec, obase))
+            dec = floor(dec / obase)
+        end while
+    end if
 
-    return {"ok", digits}
+    return {OK, digits}
 end function
